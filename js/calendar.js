25330f350f65a9ac165523255f7297e355ea1b77
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-03-07 18:18:56 -0800

    calendar

--- js/calendar.js
+++ js/calendar.js
@@ -1,16 +1,45 @@
 function myCalendar() {
   const currentYear = new Date().getFullYear();
   const currentMonth = new Date().getMonth();
-  const yearInput = document.querySelector("select[name='year']")
+  const yearInput = document.querySelector("select[name='year']");
   const monthInput = document.querySelector("select[name='month']");
   const calendar = [];
+  const daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  function getDayName(firstDay, day) {
+    return daysOfTheWeek[(firstDay + (day - 1)) % 7];
+  }
 
   function addYear(year) {
     let newYear = {};
     newYear.year = year;
-    newYear.month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    newYear.month = [
+      { days: 31 },
+      { days: 28 },
+      { days: 31 },
+      { days: 30 },
+      { days: 31 },
+      { days: 30 },
+      { days: 31 },
+      { days: 31 },
+      { days: 30 },
+      { days: 31 },
+      { days: 30 },
+      { days: 31 },
+    ];
     if (year % 4 === 0) {
-      newYear.month[1] = 29;
+      newYear.month[1].days = 29;
+    }
+    for (let i = 0; i < 12; i++) {
+      newYear.month[i].firstDay = new Date(year, i, 1).getDay();
     }
     calendar.push(newYear);
   }
@@ -31,6 +60,28 @@ function myCalendar() {
       "December",
     ];
     return months[month];
+  }
+
+  function setDays() {
+    const month = parseInt(monthInput.value);
+    const year = parseInt(yearInput.value);
+    const thisMonth = calendar.find((y) => y.year === year).month[month];
+    const days = thisMonth.days;
+    const firstDay = thisMonth.firstDay;
+    const calendarContainer = document.getElementById("calendar");
+    calendarContainer.innerHTML = "";
+    daysOfTheWeek.forEach((day) => {
+      const p = document.createElement("p");
+      p.textContent = day.substring(0, 1);
+      p.className = day;
+      calendarContainer.appendChild(p);
+    });
+    for (let i = 1; i <= days; i++) {
+      const label = document.createElement("label");
+      label.innerHTML = `${i}<input type="checkbox" name="day" value="${i}" />`;
+      label.className = getDayName(firstDay, i);
+      calendarContainer.appendChild(label);
+    }
   }
 
   for (let i = 0; i < 5; i++) {
@@ -59,18 +110,9 @@ function myCalendar() {
     monthInput.value = currentMonth;
   }
 
-  yearInput.addEventListener("change", () => {
-    monthInput.value = 0;
-    monthInput.dispatchEvent(new Event('change'));
-  });
-
-  monthInput.addEventListener("change", (e) => {
-    const month = parseInt(e.target.value);
-    const year = parseInt(yearInput.value);
-    const monthName = getMonthName(month);
-    const days = calendar.find((y) => y.year === year).month[month];
-    alert(`${monthName} has ${days} days.`);
-  });
+  yearInput.addEventListener("change", setDays);
+  monthInput.addEventListener("change", setDays);
+  document.addEventListener("DOMContentLoaded", setDays);
 }
 
 myCalendar();
