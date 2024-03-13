8fc0f75b074992ea3b198939b875d310944b5375
Mia <mia.lassiter14@gmail.com>
2024-03-12 22:52:48 -0700

    storing variables

--- php/changeEmail.php
+++ php/changeEmail.php
@@ -1,8 +1,31 @@
+//receive variables from JS 
 
-$dataArray = file_get_contents('changeEmail.json');
-$data = json_decode($dataArray, true);
+$file = 'changeEmail.json';
 
-$email =$data["email"];
+if (isset($_GET["current"])){
+    $cur = $_GET["current"];
+} else {
+    $cur = null;
+}
 
+if (isset($_GET["new"])){
+    $new = $_GET["new"];
+} else {
+    $new = null;
+}
 
-$pword = $data["password"];
+if (isset($_GET["confirm"])){
+    $confirm = $_GET["confirm"];
+} else {
+    $confirm = null;
+}
+
+if ($cur != NULL && $new != NULL && $confirm != NULL) {
+    $data = array(
+        'current'=>$cur, 
+        'new'=>$new,
+        'confirm'=>confirm
+    );
+    $str = json_encode($data);
+    file_put_contents($file, $str)
+}
--- php/changePassword.php
+++ php/changePassword.php
@@ -1,1 +1,23 @@
+//receive variables from JS 
 
+$file = 'changePassword.json';
+
+if (isset($_GET["email"])){
+    $email = $_GET["email"];
+} else {
+    $email = null;
+}
+
+if (isset($_GET["passw"])){
+    $pword = $_GET["passw"];
+} else {
+    $pword = null;
+}
+
+if ($email != NULL && $pword != NULL) {
+    $data = array(
+        'email'=>$email, 'password'=>$pword
+    );
+    $str = json_encode($data);
+    file_put_contents($file, $str)
+}
