0a4597c6e584a064c886f9960f4b318da629412c
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-03-15 19:08:16 -0700

    profile complete

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,37 @@
+// eslint-disable-next-line no-unused-vars
+async function displayProfileEvents(userID) {
+  const usersRes = await fetch("../jsons/data.json");
+  const usersJSON = await usersRes.json();
+  const eventsRes = await fetch("../jsons/info.json");
+  const eventsJSON = await eventsRes.json();
+  const events = [];
+
+  eventsJSON.forEach((obj) => {
+    if (obj.users.split(",").includes(userID)) {
+      events.push(obj.eventID);
+    }
+  });
+
+  usersJSON.forEach((obj) => {
+    if (obj.id === userID) {
+      const userName = obj.email.split("@")[0];
+      document.querySelector("#profile-name").textContent = userName;
+    }
+  });
+
+  events.forEach((event) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <h2>${eventsJSON[event - 1].name}</h2>
+      <p>Dates: ${eventsJSON[event - 1].days.map((obj) => obj.date).join(", ")}</p>
+      <button>View</button>
+      <button>Delete</button>
+    `;
+    document.querySelector("#userEvents").appendChild(div);
+  });
+}
+
 function profileSettings() {
   const profileButton = document.querySelector("header button");
   let currentState;
@@ -42,12 +76,15 @@ function profileSettings() {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
           // eslint-disable-next-line no-undef
-          changeAppState("login");
+          currentState = changeAppState("login");
         } else {
           alert("Error: " + xhr.status);
         }
       }
     };
+    profileButton
+      .querySelector("span")
+      .classList.replace("fa-xmark", "fa-bars");
   });
 }
 
