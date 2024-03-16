308b80518f8062b84b086b9abc06a27070013c42
Vladimir Ceban <70775700+v3ceban@users.noreply.github.com>
2024-03-15 19:47:51 -0700

    new account/reset password functions

--- php/newAccount.php
+++ php/newAccount.php
@@ -2,14 +2,14 @@
 
 if (isset($_POST["email"])) {
   $email = $_POST["email"];
-  // $file = 'data.json';
-  // file_put_contents($file, $email);
 }
 
 if (isset($_POST["password"])) {
   $password = $_POST["password"];
-  // $file = 'data.json';
-  // file_put_contents($file, $password);
 }
+
+// create a new object with new id in data.json (last id + 1)
+// add email and password to this object
+// create an empty array of events in this object
 
 echo $email;
--- php/resetPassword.php
+++ php/resetPassword.php
@@ -4,4 +4,8 @@ if (isset($_POST["email"])) {
   $email = $_POST["email"];
 }
 
+// find email in data.json
+// if found send email with reset form (might need to create a new html file with this form and a function that will do that)
+// (or simulate this somehow, as it's probably too hard to actually implement without a server)
+
 echo $email;
