5b90dab05f2cc38adc138927dcb34613adf30e31
Mia <mia.lassiter14@gmail.com>
2024-03-17 11:04:31 -0700

    Update newAccount.php
    add new object to json file

--- php/newAccount.php
+++ php/newAccount.php
@@ -8,8 +8,26 @@ if (isset($_POST["password"])) {
   $password = $_POST["password"];
 }
 
-// create a new object with new id in data.json (last id + 1)
-// add email and password to this object
-// create an empty array of events in this object
+$curID = 0; 
 
-echo $email;
+if (file_exists('data.json')) {
+//determine last id
+  $users = file_get_contents('data.json');
+  json_decode($users, true);
+  foreach ($users as $user) {
+    $curID++;
+  }
+}
+
+// create a new object with new id in data.json (last id + 1)
+$userData = array(
+  "id" => curID + 1,
+  "email" => $email, 
+  "password" => $password,
+  "events" => [] // create an empty array of events in this object
+);
+
+$jsonUserData = json_encode($userData);
+file_put_contents('data.json', $jsonUserData);
+
+?>
