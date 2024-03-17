d6f5b0cd4bad487712e0320ae39fffd7ba6c5d1b
Mia <mia.lassiter14@gmail.com>
2024-03-17 11:47:45 -0700

    Update newAccount.php

--- php/newAccount.php
+++ php/newAccount.php
@@ -15,15 +15,15 @@ if (file_exists('data.json')) {
   if (!empty($usersArray)) {
     $lastUser = end($usersArray);
     $lastID = $lastUser["id"]; }
+    // create a new object with new id in data.json (last id + 1)
+  $userData = array(
+    "id" => $lastID + 1,
+    "email" => $email, 
+    "password" => $password,
+    "events" => [] // create an empty array of events in this object
+  );
 }
 
-// create a new object with new id in data.json (last id + 1)
-$userData = array(
-  "id" => $lastID + 1,
-  "email" => $email, 
-  "password" => $password,
-  "events" => [] // create an empty array of events in this object
-);
 
 $jsonUserData = json_encode($userData);
 file_put_contents('data.json', $jsonUserData);
